962de8f20a3ed4077503b9760a15787bb4828b76
Dani Alamo <dca5244@psu.edu>
2023-05-03 11:01:02 -0400

    changes

--- src/merit-badge.js
+++ src/merit-badge.js
@@ -9,6 +9,7 @@ import "@lrnwebcomponents/simple-colors/simple-colors.js";
 
 class MeritBadge extends LitElement {
   static properties = {
+    ...super.properties,
     date: {type: String},
     icon: { type: String },
     title: {type: String}
@@ -16,17 +17,41 @@ class MeritBadge extends LitElement {
   }
 
   static styles = css`
-   
     .badge{
+      z-index: 5;
+      top: 10px;
+      left:10px; 
+      position: absolute; 
       width: 400px;
       height: 400px;
       border: 2px; 
       background-color: var(--simple-colors-default-theme-purple-2);
       border-radius: 50%;
       text-align : center;
-      outline: 4px dashed var(--simple-colors-default-theme-purple-2); 
+      outline: 4px dashed var(--simple-colors-default-theme-purple-2);
+      box-shadow: 0 0 0 5px var(--simple-colors-default-theme-accent-1), 1px 0px 5px 0px ;  
+      padding: 20px; 
+      margin: 20px;
 
     }
+
+    .lockBadge{
+      z-index: 6;
+      opacity: 95%;
+      position: absolute; 
+      width: 400px;
+      height: 400px;
+      border: 2px; 
+      background-color: grey;
+      border-radius: 50%;
+      text-align : center;
+      outline: 4px dashed grey;
+      box-shadow: 0 0 0 5px var(--simple-colors-default-theme-accent-1), 1px 0px 5px 0px ;  
+      padding: 20px; 
+      margin: 20px;
+
+    }
+
     .date{
       align-content: center;
       color: white; 
@@ -47,6 +72,10 @@ class MeritBadge extends LitElement {
     display: inline-block;
     vertical-align: middle;
   }
+
+  .buttonPanel{
+    margin: 20px;ss
+  }
   
   `;
 
@@ -54,7 +83,7 @@ class MeritBadge extends LitElement {
     super();
     this.date = this.getDate(); 
     this.icon = "https://static.thenounproject.com/png/1564259-200.png";
-    this.title = "A B C D E F "; 
+    this.title = "ABCDEFGHIJKLMNOP"; 
     
   }
 
@@ -66,16 +95,31 @@ class MeritBadge extends LitElement {
     return " "+ month.toString() + "/" + day.toString() + "/" + year.toString();
   }
 
+  buttonUnlock(){
+    this.locked = !this.locked;
+    if(this.locked){
+      this.shadowRoot.querySelector(".lockBadge").style.visibility='visible';
+      this.shadowRoot.querySelector(".badge").style.visibility='visible';
+    }else{
+      this.shadowRoot.querySelector(".lockBadge").style.visibility='hidden';
+      this.shadowRoot.querySelector(".badge").style.visibility='visible';
+    }
+  }
+
   render() {
     return html`
-    <div class = "badge">
 
+    <div class="lockBadge">
+     <img src="https://www.freeiconspng.com/thumbs/lock-icon/lock-icon-11.png">
+    </div>
+
+    <div class = "badge">
      <div class="date">
      <svg viewBox="0 0 500 100" class="body">
        <path id="curve" d="M73.2,222.9c4-6.1,65.5-145.2,178.6-143.4c111.3,1.2,170.8,135.5,175.1,145.5"/>
        <text width="100">
          <textPath xlink:href="#curve"startOffset="50%" text-anchor="middle">
-           ${this.title}
+           ${this.date}
          </textPath>
       </text>
       </svg> 
@@ -86,14 +130,7 @@ class MeritBadge extends LitElement {
      </div>
 
       <div class="title">
-       <svg viewBox="0 0 500 100" class="body">
-         <path id="curve2" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"/>
-          <text width="100">
-           <textPath xlink:href="#curve" startOffset="50%" text-anchor="middle">
-           ${this.date}
-          </textPath>
-          </text>
-        </svg> 
+        <h3>${this.title}<h3>  
       </div>
 
       
@@ -102,8 +139,11 @@ class MeritBadge extends LitElement {
       <simple-icon-button icon="check-circle"></simple-icon-button>
       <simple-icon-button icon="check-circle"></simple-icon-button>
       </div>
-      
 
+    </div>
+
+    <div class=buttonpanel>
+      <button @click="${this.buttonUnlock}"> Unlock Badge </button>
     </div>
     
 
